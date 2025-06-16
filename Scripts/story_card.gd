3348cd7357b5f1995ed66e649488a5ff8bbfc3c5
Jacob Sandell <13554981+Ytlander@users.noreply.github.com>
2025-06-16 15:34:34 +0200

    change can only select one card at a time
    Only one card will be selected at a time, making sure we can split up cards that are stacked.
    
    TODO: either make cards displace other cards or have cards that are put on top of other cards render on top as well as being selected first when clicked. Overall I think we need to make a rework for the card functionality

--- Scripts/story_card.gd
+++ Scripts/story_card.gd
@@ -6,18 +6,37 @@ extends Node2D
 var lerp_speed: int = 25
 
 func _on_input_event(viewport, event, shape_idx):
+	if Input.is_action_just_pressed("left_click"):
+		select_and_deselect()
+	
 	#Click to pick up, click to drop
-	if click_to_drop:
-		if Input.is_action_just_pressed("left_click"):
-			if selected == false:
-				selected = true
-			elif selected == true:
-				selected = false
+	#if click_to_drop:
+		#if Input.is_action_just_pressed("left_click"):
+			#if selected == false:
+				#selected = true
+			#elif selected == true:
+				#selected = false
 	#Click and hold to drag and and release to drop
-	if click_to_drop == false:
-		if Input.is_action_pressed("left_click"):
-			selected = true
+	#if click_to_drop == false:
+		#if Input.is_action_pressed("left_click"):
+			#selected = true
 
+func select_and_deselect():
+	if click_to_drop:
+		if click_to_drop:
+			if selected:
+				selected = false
+			else:
+				for node in get_tree().get_nodes_in_group("story_card"):
+					node.selected = false
+					
+				self.selected = true
+				
+	if !click_to_drop:
+		for node in get_tree().get_nodes_in_group("story_card"):
+			node.selected = false
+			
+		self.selected = true
 		
 func _physics_process(delta):
 	if selected:
