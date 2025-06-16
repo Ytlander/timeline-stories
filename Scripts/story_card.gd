59185f054eead2b5f00dfd7f04b38cefacd2dfeb
Jacob Sandell <13554981+Ytlander@users.noreply.github.com>
2025-06-16 08:36:44 +0200

    add hold button to drag and drop
    I wanted to test and implement this functionality too. Maybe I prefer it, I have kept both in the code so that they can be swithced by commenting the right sections

--- Scripts/story_card.gd
+++ Scripts/story_card.gd
@@ -1,14 +1,23 @@
 extends Area2D
 
 var selected: bool = false
+var lerp_speed: int = 25
 
 func _on_input_event(viewport, event, shape_idx):
-	if Input.is_action_just_pressed("left_click"):
-		if selected == false:
-			selected = true
-		elif selected == true:
-			selected = false
+	#Click to pick up, click to drop
+	#if Input.is_action_just_pressed("left_click"):
+		#if selected == false:
+			#selected = true
+		#elif selected == true:
+			#selected = false
+	#Click and hold to drag and and release to drop
+	if Input.is_action_pressed("left_click"):
+		selected = true
+
 		
 func _physics_process(delta):
 	if selected:
-		self.global_position = lerp(self.global_position, get_global_mouse_position(), 25 * delta)
+		self.global_position = lerp(self.global_position, get_global_mouse_position(), lerp_speed * delta)
+		#Click and hold functionality
+		if Input.is_action_just_released("left_click"):
+			selected = false
