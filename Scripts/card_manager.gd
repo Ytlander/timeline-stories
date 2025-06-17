d263a5e41304039a66fb7037bced30a88475f0fa
Jacob Sandell <13554981+Ytlander@users.noreply.github.com>
2025-06-17 09:42:17 +0200

    add mouse offset and hidden
    The card doesn't snap to the middle anymore but stays offset relative to the mouse when the card is grabbed. The mouse cursor is also hidden when dragging a card.

--- Scripts/card_manager.gd
+++ Scripts/card_manager.gd
@@ -5,22 +5,27 @@ extends Node2D
 var clicked_cards: Array
 var dragged_card: Area2D
 var lerp_speed: int = 25
+var mouse_offset: Vector2
 
 func _ready():
 	EventBus.card_clicked.connect(_on_card_clicked)
 
 func _physics_process(delta):
 		if dragged_card != null:
+			Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 			check_overlapping_cards()
-			dragged_card.global_position = lerp(dragged_card.global_position, get_global_mouse_position(), lerp_speed * delta)
+			var target_position = get_global_mouse_position() - mouse_offset
+			dragged_card.global_position = lerp(dragged_card.global_position, target_position, lerp_speed * delta)
 		
 		if click_to_drop == false:
 			if Input.is_action_just_released("left_click"):
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 				dragged_card = null
 				clicked_cards.clear()
 		
 		if Input.is_action_just_pressed("left_click"):
 			if click_to_drop && dragged_card != null:
+				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 				dragged_card = null
 				clicked_cards.clear()
 
@@ -43,11 +48,17 @@ func _input(event):
 				dragged_card = get_highest_z_level_card(clicked_cards)
 			else:
 				dragged_card = clicked_cards[0]
+		## Getting the mouse offset so that card doesn't snap to the center
+		if dragged_card:
+			var mouse_position = get_global_mouse_position()
+			mouse_offset = mouse_position - dragged_card.global_position
 
+##This sorts an array based on Z index. Maybe move this into a utility function in the future? Could be used for multiple things. 
 func get_highest_z_level_card(cards: Array):
 	cards.sort_custom(func(a, b): return a.z_index > b.z_index)
 	
 	return cards[0]
-	
+
+##This will fire once for every overlapping card that is clicked on. Therefore we save all the cards in an array	
 func _on_card_clicked(card):
 	clicked_cards.append(card)
--- Scripts/story_card.gd
+++ Scripts/story_card.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-
+##Tells the Card Manager that the card has been clicked
 func _on_input_event(viewport, event, shape_idx):
 	if Input.is_action_just_pressed("left_click"):
 		EventBus.card_clicked.emit(self)
