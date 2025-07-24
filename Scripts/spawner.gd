9a24677e67ead3f4f5db7e2c52921c9d9bba05e0
Jacob Sandell <13554981+Ytlander@users.noreply.github.com>
2025-07-24 16:45:29 +0200

    add spawner
    There is a working spawner but there is still some work to be done
    
    todo: fix so that cards spawn in different positions
    Fix array index variables

---
+++ Scripts/spawner.gd
@@ -0,0 +1,35 @@
+extends Node
+
+
+
+@export var batch_1: Array[PackedScene]
+@export var batch_2: Array[PackedScene]
+@export var batch_3: Array[PackedScene]
+@export var batch_4: Array[PackedScene]
+@export var batch_5: Array[PackedScene]
+@export var batch_6: Array[PackedScene]
+
+var batch_array: Array[Array]
+
+var spawn_position_array: Array[Node2D]
+
+@onready var spawn_position = $SpawnPosition
+@onready var spawn_position_2 = $SpawnPosition2
+@onready var spawn_position_3 = $SpawnPosition3
+@onready var spawn_position_4 = $SpawnPosition4
+
+
+func _ready():
+	EventBus.validate_successful.connect(_on_validate_successful)
+	batch_array = [batch_1, batch_2, batch_3, batch_4, batch_5, batch_6]
+	spawn_position_array = [spawn_position, spawn_position_2, spawn_position_3, spawn_position_4]
+	
+func _on_validate_successful(batch_to_spawn):
+	var current_batch = batch_array[batch_to_spawn - 1]
+	for packed_card in current_batch:
+		var card_number = 0
+		var card = packed_card.instantiate()
+		self.add_child(card)
+		print(spawn_position_array)
+		card.global_position = spawn_position_array[card_number].global_position
+		card_number += 1
