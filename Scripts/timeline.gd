b7f8454199910da098c1c9bca0f07d1d04a59915
Jacob Sandell <13554981+Ytlander@users.noreply.github.com>
2025-07-25 14:27:12 +0200

    add batch 4, 5 and 6
    Added the last batches to the game.
    
    Added a win message when completing the whole timeline

--- Scripts/timeline.gd
+++ Scripts/timeline.gd
@@ -101,8 +101,9 @@ func _on_check_button_pressed() -> void:
 			show_message("Wrong order, try again!", "Incorrect")
 			return
 		if placed_card == placed_cards.back():
-			#The success message is now handled by the reading area
-			#show_message("Good job, you placed the cards in the correct order!", "Success")
+			if card_batch == 6:
+				show_message("Good job, you placed all the cards in the correct order!", "Win")
+				return
 			EventBus.validate_successful.emit(card_batch)
 			card_batch += 1
 			return
